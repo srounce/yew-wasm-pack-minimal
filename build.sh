4d5e32d4ff8a96908d392208974ab4b7256684ca
Samuel Rounce <me@samuelrounce.co.uk>
2019-08-15 00:26:29 +0100

    Allow passing of arguments to build script
    `build.sh` expects `target` and  `format` args to be passed to
    `wasm-pack`'s `target` flag and `rollup`'s `format` flag respectively

--- build.sh
+++ build.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env sh
 
-wasm-pack build -t web && rollup ./src/index.js --file ./dist/bundle.js --format cjs
+set -e
+
+TARGET_ARG=$1
+TARGET=${TARGET_ARG:=web}
+
+FORMAT_ARG=$2
+FORMAT=${FORMAT_ARG:=iife}
+
+echo "Using target \`${TARGET}\`"
+echo "Using format \`${FORMAT}\`"
+
+wasm-pack build -t $TARGET
+rollup ./src/index --file ./pkg/bundle.js --format $FORMAT
 mkdir -p dist
 cp -v pkg/*.wasm dist/
 cp -rv static/* dist/
